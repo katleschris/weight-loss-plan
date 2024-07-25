e5aef5f39d0f14792e3bf97637ae0e1cc3872e12
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-07-25 12:48:50 +0200

    Seperated grocery list according to store

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -4,62 +4,51 @@ import React from 'react';
 const GroceryList = () => (
   <div>
     <h2>Weekly Grocery List</h2>
-    <h3>Protein Sources</h3>
+    <h3>Pick n Pay</h3>
     <ul>
-      <li>Chicken breasts (boneless, skinless) - 2 lbs (PnP)</li>
-      <li>Fish fillets (salmon, tilapia, etc.) - 1 lb(PnP)</li>
-      <li>Tofu - 1-2 packs(Woolworths)</li>
-      <li>Eggs - 1 dozen(Britos)</li>
-      <li>Greek yogurt - 2 large tubs (PnP)</li>
-      <li>Tuna (canned in water) - 2 cans (PnP)</li>
-      <li>Cottage cheese - 1 large tub(Woolworths)</li>
-      <li>Protein powder (optional for smoothies) - 1 small container(PnP)</li>
+      <li>Chicken breasts (boneless, skinless) - 2 lbs</li>
+      <li>Fish fillets (salmon, tilapia, etc.) - 1 lb</li>
+      <li>Greek yogurt - 2 large tubs</li>
+      <li>Tuna (canned in water) - 2 cans</li>
+      <li>Protein powder (optional for smoothies) - 1 small container</li>
+      <li>Cherry tomatoes - 1 pint</li>
+      <li>Red onions - 2</li>
+      <li>Berries (strawberries, blueberries, etc.) - 2 pints</li>
+      <li>Mixed berries (for snacks) - 1 pint</li>
+      <li>Quinoa - 1 lb</li>
+      <li>Brown rice - 1 lb</li>
+      <li>Almond butter - 1 jar</li>
+      <li>Chia seeds - 1 small bag</li>
+      <li>Dark chocolate (70% cocoa or higher) - 1 bar</li>
+      <li>Almond milk (for smoothies) - 1 carton</li>
     </ul>
-    <h3>Vegetables</h3>
+    <h3>Woolworths</h3>
     <ul>
-      <li>Spinach - 1-2 bags (street)</li>
-      <li>Cherry tomatoes - 1 pint (PnP)</li>
-      <li>Cucumbers - 2 (woolworths)</li>
-      <li>Red onions - 2 (PnP)</li>
+      <li>Tofu - 1-2 packs</li>
+      <li>Cottage cheese - 1 large tub</li>
+      <li>Cucumbers - 2</li>
+      <li>Carrots - 1 lb</li>
+      <li>Broccoli - 1 large head</li>
+      <li>Zucchini/Baby marrows - 2</li>
+      <li>Celery - 1 bunch</li>
+      <li>Lettuce (for tuna salad) - 1 head</li>
+      <li>Bananas - 6</li>
+      <li>Pineapple (fresh or canned) - 1</li>
+      <li>Almonds - 0.5 lb</li>
+      <li>Walnuts - 0.5 lb</li>
+      <li>Olive oil - 1 bottle</li>
+    </ul>
+    <h3>Other Sources</h3>
+    <ul>
+      <li>Eggs - 1 dozen (Britos)</li>
+      <li>Spinach - 1-2 bags (Street)</li>
+      <li>Bell peppers - 2 (Street)</li>
       <li>Avocados - 3 (Shoprite)</li>
-      <li>Carrots - 1 lb(woolworths)</li>
-      <li>Broccoli - 1 large head (woolworths)</li>
-      <li>Zucchini/ babymarrows  - 2 (woolworths)</li>
-      <li>Bell peppers - 2 (street)</li>
-      <li>Celery - 1 bunch (woolworths)</li>
-      <li>Lettuce (for tuna salad) - 1 head (woolworths)</li>
-    </ul>
-    <h3>Fruits</h3>
-    <ul>
-      <li>Bananas - 6 (woolworths)</li>
-      <li>Apples - 4 (street)</li>
-      <li>Berries (strawberries, blueberries, etc.) - 2 pints(PnP)</li>
-      <li>Pineapple (fresh or canned) (woolworths)- 1</li>
-      <li>Mixed berries (for snacks) - 1 pint(PnP)</li>
-    </ul>
-    <h3>Whole Grains</h3>
-    <ul>
-      <li>Quinoa - 1 lb(PnP)</li>
-      <li>Brown rice - 1 lb(PnP)</li>
-    </ul>
-    <h3>Healthy Fats</h3>
-    <ul>
-      <li>Almonds - 0.5 lb (woolworths)</li>
-      <li>Walnuts - 0.5 lb (woolworths)</li>
+      <li>Apples - 4 (Street)</li>
       <li>Peanut butter - 1 jar</li>
-      <li>Almond butter - 1 jar (PnP)</li>
-      <li>Chia seeds - 1 small bag (PnP/ Woolworths)</li>
-      <li>Olive oil - 1 bottle (Woolworths)</li>
-    </ul>
-    <h3>Snacks and Miscellaneous</h3>
-    <ul>
-      <li>Dark chocolate (70% cocoa or higher) - 1 bar (PnP)</li>
+      <li>Chia seeds - 1 small bag (PnP/Woolworths)</li>
       <li>Herbal tea - 1 box</li>
-    </ul>
-    <h3>Beverages</h3>
-    <ul>
       <li>Water (if not using tap or filtered) - enough to cover daily needs</li>
-      <li>Almond milk (for smoothies) - 1 carton (PnP)</li>
     </ul>
   </div>
 );
